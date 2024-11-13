1a2f2c495a518e54f7153e33426fc9a941d015fc
Karlo Cehulic <karlocehlic@gmail.com>
2024-11-13 18:40:45 +0100

    Add characters and their positions in db

---
+++ backend/prisma/seed.js
@@ -0,0 +1,48 @@
+const { PrismaClient } = require("@prisma/client");
+const client = new PrismaClient();
+
+const characters = [
+  {
+    name: "Waldo",
+    positionL: 60.5,
+    positionT: 34.1,
+    widthPer: 2.6,
+    heightPer: 4.1,
+  },
+  {
+    name: "Wizard",
+    positionL: 25.6,
+    positionT: 31.4,
+    widthPer: 2.6,
+    heightPer: 4.1,
+  },
+  {
+    name: "Odlaw",
+    positionL: 10.2,
+    positionT: 17.7,
+    widthPer: 1.3,
+    heightPer: 4.1,
+  },
+];
+
+async function main() {
+  for (char of characters) {
+    await client.character.upsert({
+      where: {
+        name: char.name,
+      },
+      update: {},
+      create: {
+        ...char,
+      },
+    });
+  }
+}
+
+main()
+  .then(async () => await client.$disconnect())
+  .catch(async (err) => {
+    console.error(err);
+    await client.$disconnect();
+    process.exit(1);
+  });
