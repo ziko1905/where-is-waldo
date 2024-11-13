3d9f78c48a40429b5b3dab38f96deb6c527be3d9
Karlo Cehulic <karlocehlic@gmail.com>
2024-11-13 19:38:10 +0100

    Add session storing

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,30 @@
 const express = require("express");
 const app = express();
 const searchRouter = require("./routes/searchRouter");
+const expressSession = require("express-session");
+const {
+  prismaSessionStore,
+  PrismaSessionStore,
+} = require("@quixo3/prisma-session-store");
+const { PrismaClient } = require("@prisma/client");
+const passport = require("passport");
+
+app.use(
+  expressSession({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 2,
+    },
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 2 * 60 * 1000,
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
+app.use(passport.session());
 
 app.use("/search", searchRouter);
 
