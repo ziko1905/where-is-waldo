ccddb724970a292340220156b02d3ca737d6a79c
Karlo Cehulic <karlocehlic@gmail.com>
2024-11-27 17:55:33 +0100

    Add stlying to leaderboard

--- frontend/src/components/Leaderboard.jsx
+++ frontend/src/components/Leaderboard.jsx
@@ -1,19 +1,23 @@
 import PropTypes from "prop-types";
 import PlayerCard from "./partials/PlayerCard";
 import convertMSToStr from "../utils/convertMSToStr";
+import "../styles/components/Leaderboard.css";
 
 export default function Leaderboard({ lb = [], sessionTime }) {
   return (
     <>
       <div className="lb">
-        <span>Your Time: {convertMSToStr(sessionTime)}</span>
-        {!!lb.length &&
-          lb.map((ply, index) => {
-            const position = index + 1;
-            return (
-              <PlayerCard key={crypto.randomUUID()} {...ply} pos={position} />
-            );
-          })}
+        <span id="player-time">Your Time: {convertMSToStr(sessionTime)}</span>
+        <div className="results">
+          {!!lb.length &&
+            lb.map((ply, index) => {
+              const position = index + 1;
+              return (
+                <PlayerCard key={crypto.randomUUID()} {...ply} pos={position} />
+              );
+            })}
+        </div>
+        <span id={"greeting-span"}>Thank you for playing!</span>
       </div>
     </>
   );
